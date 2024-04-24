d56e5114f7c9d2cdb265e73d552dd24899632079
Felipe da Silva <silvaafelipe016@gmail.com>
2024-04-24 17:18:25 -0300

    feat: changes in the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,43 +1,71 @@
 import styled from "styled-components";
 
-const Container = styled.nav`
+import { IconMenu } from "./icons";
+
+const Container = styled.header`
   display: flex;
   width: 100%;
-  height: 100%;
-  padding: 15px 75px;
+  height: 5.5em;
+  position: absolute;
+  top: 0;
   align-items: center;
-  justify-content: space-between;
+  justify-content: center;
 
-  & > span {
-    font-size: 24px;
-    font-weight: 700;
-  }
-
-  ul {
+  nav {
     display: flex;
-    gap: 30px;
-    flex-direction: row;
+    width: 90%;
     align-items: center;
-    justify-content: center;
+    justify-content: space-between;
 
-    & > li {
-      list-style: none;
-      font-size: 18px;
-      font-weight: 500;
+    & > span {
+      color: #fff;
+      font-size: 36px;
+      font-weight: 700;
+    }
+
+    ul {
+      display: flex;
+      gap: 30px;
+      flex-direction: row;
+      align-items: center;
+      justify-content: center;
+      & > li {
+        color: #fff;
+        font-size: 18px;
+        font-weight: 500;
+        list-style: none;
+      }
+    }
+
+    & > svg {
+      display: none;
+      color: #fff;
+    }
+
+    @media (max-width: 768px) {
+      ul {
+        display: none;
+      }
+
+      & > svg {
+        display: flex;
+      }
     }
   }
 `;
-
 export const Navbar = () => {
   return (
     <Container>
-      <span>Pet Beauty</span>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Services</li>
-        <li>Contact</li>
-      </ul>
+      <nav>
+        <span>Pet Beauty</span>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Contact</li>
+        </ul>
+        <IconMenu />
+      </nav>
     </Container>
   );
 };
---
+++ src/components/icons/Menu.tsx
@@ -0,0 +1,15 @@
+export const IconMenu = () => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="45"
+    height="45"
+    viewBox="0 0 20 20"
+  >
+    <path
+      fill="currentColor"
+      fill-rule="evenodd"
+      d="M3 7a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1m0 6a1 1 0 0 1 1-1h12a1 1 0 1 1 0 2H4a1 1 0 0 1-1-1"
+      clip-rule="evenodd"
+    />
+  </svg>
+);

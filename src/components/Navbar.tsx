8475597ac4552d26a35f8e24d8242cece9cc7d3c
Felipe da Silva <silvaafelipe016@gmail.com>
2024-04-28 23:56:49 -0300

    feat: added responsiveness to navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,17 @@
+import { useEffect, useState } from "react";
+
 import styled from "styled-components";
 
-import { IconMenu } from "./icons";
+import { IconClose, IconMenu } from "./icons";
 
-const Container = styled.header`
+interface IHeaderProps {
+  isOpen: boolean;
+}
+
+const StyledHeader = styled.header<IHeaderProps>`
   display: flex;
   width: 100%;
-  height: 5.5em;
+  height: 6em;
   position: absolute;
   top: 0;
   align-items: center;
@@ -31,17 +37,28 @@ const Container = styled.header`
       align-items: center;
       justify-content: center;
 
-      & > li > a {
-        color: #fff;
-        font-size: 18px;
-        font-weight: 500;
-        text-decoration: none;
+      & > li {
+        height: 30px;
+
+        a {
+          color: #fff;
+          font-size: 18px;
+          font-weight: 500;
+          text-decoration: none;
+        }
+
+        &:hover {
+          border-bottom: 2px solid #fff;
+        }
       }
     }
 
-    & > svg {
+    & > button {
       display: none;
+      z-index: 2;
       color: #fff;
+      cursor: pointer;
+      background: none;
     }
 
     @media (max-width: 768px) {
@@ -49,15 +66,91 @@ const Container = styled.header`
         display: none;
       }
 
-      & > svg {
+      & > button {
+        display: flex;
+      }
+    }
+  }
+
+  div {
+    display: none;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    right: 0;
+    bottom: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    visibility: ${({ isOpen }) => (isOpen ? "visible" : "hidden")};
+    transition: visibility 0.3s ease-in-out;
+
+    & > aside {
+      position: fixed;
+      width: min(70%, 400px);
+      height: 100%;
+      padding: 20em 0;
+      right: ${({ isOpen }) => (isOpen ? "0" : "-100%")};
+      align-items: center;
+      justify-content: center;
+      background-color: #ff8800;
+      border-radius: 15px 0 0 15px;
+      visibility: ${({ isOpen }) => (isOpen ? "visible" : "hidden")};
+      transition: 0.3s ease-in-out;
+
+      ul {
+        display: flex;
+        gap: 30px;
+        list-style: none;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+
+        & > li {
+          height: 30px;
+
+          a {
+            color: #fff;
+            font-size: 22px;
+            font-weight: 500;
+            text-decoration: none;
+          }
+
+          &:hover {
+            border-bottom: 2px solid #fff;
+          }
+        }
+      }
+
+      @media (max-width: 768px) {
+        & {
+          display: flex;
+        }
+      }
+    }
+
+    @media (max-width: 768px) {
+      & {
         display: flex;
       }
     }
   }
 `;
 export const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleScroll = () => {
+    document.body.style.overflow = isOpen ? "hidden" : "auto";
+  };
+
+  useEffect(() => {
+    toggleScroll();
+
+    return () => {
+      document.body.style.overflow = "auto";
+    };
+  }, [isOpen]);
+
   return (
-    <Container>
+    <StyledHeader isOpen={isOpen}>
       <nav>
         <span>Pet Beauty</span>
         <ul>
@@ -74,8 +167,34 @@ export const Navbar = () => {
             <a href="#contact">Contact</a>
           </li>
         </ul>
-        <IconMenu />
+        {isOpen ? (
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <IconClose />
+          </button>
+        ) : (
+          <button onClick={() => setIsOpen(!isOpen)}>
+            <IconMenu />
+          </button>
+        )}
       </nav>
-    </Container>
+      <div onClick={() => setIsOpen(false)}>
+        <aside>
+          <ul>
+            <li>
+              <a href="#">Home</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </aside>
+      </div>
+    </StyledHeader>
   );
 };
--- src/components/icons/index.tsx
+++ src/components/icons/index.tsx
@@ -1,4 +1,5 @@
 export * from "./Menu";
+export * from "./Close";
 export * from "./Twitter";
 export * from "./Facebook";
 export * from "./Instagram";

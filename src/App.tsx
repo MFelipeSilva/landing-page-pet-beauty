8284d36b1ae2df4c3bd56b4be6c3685f1328eae1
Felipe da Silva <silvaafelipe016@gmail.com>
2024-04-29 10:00:49 -0300

    feat: changes in the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,52 +8,8 @@ import Contact from "./pages/contact";
 const Container = styled.main`
   display: flex;
   width: 100%;
-<<<<<<< HEAD
   height: 100%;
   flex-direction: column;
-=======
-  height: 100vh;
-  padding-left: 5.1vw;
-  align-items: center;
-  justify-content: start;
-  background-image: url(${banner});
-  background-size: cover;
-  background-repeat: no-repeat;
-
-  div {
-    display: flex;
-    max-width: 660px;
-    width: 60%;
-    gap: 30px;
-    flex-direction: column;
-
-    h1 {
-      color: #fff;
-      font-size: clamp(45px, 4vw, 65px);
-
-      & > span {
-        color: #3d348b;
-      }
-    }
-
-    @media (max-width: 768px) {
-      width: 70%;
-    }
-
-    @media (max-width: 520px) {
-      width: 90%;
-    }
-  }
-
-  @media (max-width: 1440px) {
-    background-position: center;
-  }
-
-  @media (max-width: 520px) {
-    padding-left: 0;
-    justify-content: center;
-  }
->>>>>>> f69ccf7 (feat: changes in home page)
 `;
 
 function App() {
